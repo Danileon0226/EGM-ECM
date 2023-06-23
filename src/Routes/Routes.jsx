36599ab077077575a29622afef74b5b5b00f90bf
DaniLeon <122113337+Danileon0226@users.noreply.github.com>
2023-06-23 13:01:28 -0500

    Firebase
    Inyeccion de dependencias de Firebase,Firebase App, Firebase Firestore

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,11 @@
+import Carrito from "../Pages/Carrito";
 import Home from "../Pages/Home";
 
 const routes = [
   { path: "*", component: Home },
   { path: "/", component: Home },
   { path: "Inicio", component: Home },
+  {path: 'Carrito', componen: Carrito}
 ];
 
 export { routes };
