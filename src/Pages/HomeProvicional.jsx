c8024a4432b7559131e5a2aeff8e5e28359b1f02
SamuelVasquezGonzalez <samuegonza2006@gmail.com>
2023-06-23 11:42:23 -0500

    Se agrego, redux/redux toolik, FontAwesome Icons, ReactRouterDom, se inicio una estructura basica y se organizaron las carpetas

---
+++ src/Pages/HomeProvicional.jsx
@@ -0,0 +1,5 @@
+export default function HomeProvicional() {
+    return (
+        <div>Hola Mundo</div>
+    )
+}
---
+++ src/Store/Store.jsx
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+    reducer:{
+        // Todos los slices van aca
+    }
+})
